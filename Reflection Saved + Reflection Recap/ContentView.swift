4770e81b3d8c77493cb95dd9df5003d13e992121
Yuwen Lu <yuwen_lu@outlook.com>
2025-02-25 10:37:32 +1100

    Initial Commit

---
+++ Reflection Saved + Reflection Recap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reflection Saved + Reflection Recap
+//
+//  Created by Yuwen Lu on 25/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
